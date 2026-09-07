470e1dc4bf4a55a685590402afc9628c5bfddfce
Dave Barton <8168910+gofastercloud@users.noreply.github.com>
2026-09-07 15:11:34 +1000

    Show bounded ImageBuilder prerequisite progress
    Keep the official ImageBuilder check output visible through native firewall apply so missing Host prerequisites are actionable while credentials remain on stdin.

--- scripts/build-openwrt-firewall.sh
+++ scripts/build-openwrt-firewall.sh
@@ -139,7 +139,7 @@ EOF
 
 packages='uhttpd uhttpd-mod-ubus rpcd rpcd-mod-file rpcd-mod-iwinfo px5g-mbedtls ca-bundle firewall4 nftables qemu-ga'
 log "building generic x86/64 image"
-make -C "$builder" image PROFILE=generic PACKAGES="$packages" FILES="$files" >/dev/null
+make -C "$builder" image PROFILE=generic PACKAGES="$packages" FILES="$files"
 log "locating generic ext4 combined image"
 source_image=$(find "$builder/bin/targets/x86/64" -type f -name '*generic-ext4-combined.img.gz' -print -quit)
 test -n "$source_image"
